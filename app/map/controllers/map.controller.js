aee6c8e2b0472d82ba8fca57da5158f56aeb79ba
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-09-07 22:57:20 -0700

    First revision of google map controller and view

--- app/map/controllers/map.controller.js
+++ app/map/controllers/map.controller.js
@@ -1,9 +1,36 @@
 angular.module('chaseApp')
   .controller('MapCtrl', function ($scope, Map) {
-    $scope.geoLocation = Map.geoLocation.coords;
+    var userLocation = Map.geoLocation.coords;
+    $scope.userLat = userLocation.latitude;
+    $scope.userLng = userLocation.longitude;
 
-    Map.getLocations($scope.geoLocation.latitude, $scope.geoLocation.longitude)
+    Map.getLocations($scope.userLat, $scope.userLng)
       .then(function(results) {
         $scope.chaseLocations = results.data.locations;
+        var current = 'A'.charCodeAt();
+        console.log($scope.chaseLocations)
+        _.each($scope.chaseLocations, function(item) {
+          var letter = String.fromCharCode(current++);
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item.lat, item.lng),
+            icon: 'assets/images/google_map_markers/blue_Marker' + letter + '.png',
+            map: map,
+            title: 'Chase - ' + item.name
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            alert('hello');
+          });
+        })
       });
+
+    var mapOptions = {
+      center: new google.maps.LatLng($scope.userLat, $scope.userLng),
+      zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var marker = new google.maps.Marker({
+      position: mapOptions.center,
+      map: map,
+      title: 'Current Location'
+    });
   });
