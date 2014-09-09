e0c32b5a92b161a678674da3e428f79402c7635d
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-09-08 18:29:22 -0700

    Add watch task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,22 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    appPath: {
-      client: require('./bower.json').appPath || 'app',
-      dist: 'dist'
-    },
     watch: {
-      injectJS: {
-        files: [
-          '<%= appPath.client %>/{app,components}/**/*.js',
-          '!<%= appPath.client %>/{app,components}/**/*.spec.js',
-          '!<%= appPath.client %>/{app,components}/**/*.mock.js',
-          '!<%= appPath.client %>/app/app.js'],
-        tasks: ['injector:scripts']
-      },
-      injectCss: {
-        files: [
-          '<%= appPath.client %>/{app,components}/**/*.css'
-        ],
-        tasks: ['injector:css']
-      },
-      mochaTest: {
-        files: ['server/**/*.spec.js'],
-        tasks: ['env:test', 'mochaTest']
-      },
-      livereload: {
-        files: [
-          '{.tmp,<%= appPath.client %>}/{app,components}/**/*.css',
-          '{.tmp,<%= appPath.client %>}/{app,components}/**/*.html',
-          '{.tmp,<%= appPath.client %>}/{app,components}/**/*.js',
-          '!{.tmp,<%= appPath.client %>}{app,components}/**/*.spec.js',
-          '!{.tmp,<%= appPath.client %>}/{app,components}/**/*.mock.js'
-        ],
-        options: {
-          livereload: true
-        }
-      }
-    },
-    uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        livereload: true
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      js: {
+        files: [
+          'app/**/*.js',
+          'app/**/*.spec.js'],
+      },
+      css: {
+        files: [
+          'app/**/*.css'
+        ]
+      },
+      karmaTest: {
+        files: ['app/**/*.spec.js']
       }
     }
   });
