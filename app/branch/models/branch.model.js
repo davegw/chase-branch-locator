41eb43c773b56966f844f0da65d94fc54ec09108
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-09-08 19:06:39 -0700

    Persist selected branch on page refresh

--- app/branch/models/branch.model.js
+++ app/branch/models/branch.model.js
@@ -4,10 +4,15 @@ angular.module('app.model.branch', [])
 
     branchInstance.selectBranch = function(branch) {
       this.currentBranch = branch;
+
+      // Store selected branch in local storage in case page gets refreshed.
+      window.localStorage.setItem('selectedBranch', JSON.stringify(branch));
     };
 
     branchInstance.getBranch = function(branch) {
-      return this.currentBranch;
+      // Return the current Branch or search in local storage if branch is not defined.
+      var branch = this.currentBranch || JSON.parse(localStorage.getItem('selectedBranch'));
+      return branch;
     };
 
     return branchInstance;
