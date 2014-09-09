9f77c312b535aca4e3b64bd6de11f48c3858900b
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-09-08 22:20:45 -0700

    Refactor map controller

--- app/map/controllers/map.controller.js
+++ app/map/controllers/map.controller.js
@@ -4,35 +4,57 @@ angular.module('chaseApp')
     $scope.userLat = userLocation.latitude;
     $scope.userLng = userLocation.longitude;
 
+    // Get nearby branches based on user location and set them on the scope.
     Map.getLocations($scope.userLat, $scope.userLng)
       .then(function(results) {
-        $scope.chaseLocations = results.data.locations;
-        var current = 'A'.charCodeAt();
-        console.log($scope.chaseLocations)
-        _.each($scope.chaseLocations, function(item) {
-          var letter = String.fromCharCode(current++);
-          var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(item.lat, item.lng),
-            icon: 'assets/images/google_map_markers/blue_Marker' + letter + '.png',
-            map: map,
-            title: 'Chase - ' + item.name,
-            info: item
-          });
-          google.maps.event.addListener(marker, 'click', function() {
-            Branch.selectBranch(this.info);
-            $state.go('branch');
-          });
-        })
+        identifyNearbyBranches(results.data.locations);
       });
 
+    // Create a map based on user's location.
     var mapOptions = {
       center: new google.maps.LatLng($scope.userLat, $scope.userLng),
       zoom: 15
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    // Create marker for user's location.
     var marker = new google.maps.Marker({
       position: mapOptions.center,
       map: map,
       title: 'Current Location'
     });
+
+
+    // For each nearby branch create a google marker and click event listener.
+    function identifyNearbyBranches(branchArray) {
+      $scope.chaseLocations = branchArray;
+      var current = 'A'.charCodeAt();
+      console.log($scope.chaseLocations);
+
+      _.each($scope.chaseLocations, function(branch) {
+        var letter = String.fromCharCode(current++);
+        var marker = createBranchMarker(branch, letter);
+        branchMarkerClickHandler(marker);
+      });
+    };
+
+    // Create a marker for each branch location.
+    function createBranchMarker(branch, letter) {
+      return new google.maps.Marker({
+        position: new google.maps.LatLng(branch.lat, branch.lng),
+        icon: 'assets/images/google_map_markers/blue_Marker' + letter + '.png',
+        map: map,
+        title: 'Chase - ' + branch.name,
+        info: branch
+      });
+    }
+
+    // Click event handler for each marker.
+    function branchMarkerClickHandler(branchMarker) {
+      google.maps.event.addListener(branchMarker, 'click', function() {
+        // Persist the selected Branch info using the Branch factory.
+        Branch.selectBranch(this.info);
+        $state.go('root.branch');
+      });
+    }
   });
